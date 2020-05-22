c167f453b659187fb6ce2e9e7be8f0034382899d
Nathan Kleekamp <nkleekamp@gmail.com>
2020-05-22 14:53:14 -0400

    v0.41.0

---
+++ app.js
@@ -0,0 +1,333 @@
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+
+// This is a specialised implementation of a System module loader.
+
+// @ts-nocheck
+/* eslint-disable */
+let System, __instantiateAsync, __instantiate;
+
+(() => {
+  const r = new Map();
+
+  System = {
+    register(id, d, f) {
+      r.set(id, { d, f, exp: {} });
+    },
+  };
+
+  async function dI(mid, src) {
+    let id = mid.replace(/\.\w+$/i, "");
+    if (id.includes("./")) {
+      const [o, ...ia] = id.split("/").reverse(),
+        [, ...sa] = src.split("/").reverse(),
+        oa = [o];
+      let s = 0,
+        i;
+      while ((i = ia.shift())) {
+        if (i === "..") s++;
+        else if (i === ".") break;
+        else oa.push(i);
+      }
+      if (s < sa.length) oa.push(...sa.slice(s));
+      id = oa.reverse().join("/");
+    }
+    return r.has(id) ? gExpA(id) : import(mid);
+  }
+
+  function gC(id, main) {
+    return {
+      id,
+      import: (m) => dI(m, id),
+      meta: { url: id, main },
+    };
+  }
+
+  function gE(exp) {
+    return (id, v) => {
+      v = typeof id === "string" ? { [id]: v } : id;
+      for (const [id, value] of Object.entries(v)) {
+        Object.defineProperty(exp, id, {
+          value,
+          writable: true,
+          enumerable: true,
+        });
+      }
+    };
+  }
+
+  function rF(main) {
+    for (const [id, m] of r.entries()) {
+      const { f, exp } = m;
+      const { execute: e, setters: s } = f(gE(exp), gC(id, id === main));
+      delete m.f;
+      m.e = e;
+      m.s = s;
+    }
+  }
+
+  async function gExpA(id) {
+    if (!r.has(id)) return;
+    const m = r.get(id);
+    if (m.s) {
+      const { d, e, s } = m;
+      delete m.s;
+      delete m.e;
+      for (let i = 0; i < s.length; i++) s[i](await gExpA(d[i]));
+      const r = e();
+      if (r) await r;
+    }
+    return m.exp;
+  }
+
+  function gExp(id) {
+    if (!r.has(id)) return;
+    const m = r.get(id);
+    if (m.s) {
+      const { d, e, s } = m;
+      delete m.s;
+      delete m.e;
+      for (let i = 0; i < s.length; i++) s[i](gExp(d[i]));
+      e();
+    }
+    return m.exp;
+  }
+
+  __instantiateAsync = async (m) => {
+    System = __instantiateAsync = __instantiate = undefined;
+    rF(m);
+    return gExpA(m);
+  };
+
+  __instantiate = (m) => {
+    System = __instantiateAsync = __instantiate = undefined;
+    rF(m);
+    return gExp(m);
+  };
+})();
+
+System.register(
+  "https://deno.land/std@0.41.0/uuid/_common",
+  [],
+  function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+    function bytesToUuid(bytes) {
+      const bits = [...bytes].map((bit) => {
+        const s = bit.toString(16);
+        return bit < 0x10 ? "0" + s : s;
+      });
+      return [
+        ...bits.slice(0, 4),
+        "-",
+        ...bits.slice(4, 6),
+        "-",
+        ...bits.slice(6, 8),
+        "-",
+        ...bits.slice(8, 10),
+        "-",
+        ...bits.slice(10),
+      ].join("");
+    }
+    exports_1("bytesToUuid", bytesToUuid);
+    return {
+      setters: [],
+      execute: function () {
+      },
+    };
+  },
+);
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+System.register(
+  "https://deno.land/std@0.41.0/uuid/v1",
+  ["https://deno.land/std@0.41.0/uuid/_common"],
+  function (exports_2, context_2) {
+    "use strict";
+    var _common_ts_1, UUID_RE, _nodeId, _clockseq, _lastMSecs, _lastNSecs;
+    var __moduleName = context_2 && context_2.id;
+    function validate(id) {
+      return UUID_RE.test(id);
+    }
+    exports_2("validate", validate);
+    function generate(options, buf, offset) {
+      let i = (buf && offset) || 0;
+      const b = buf || [];
+      options = options || {};
+      let node = options.node || _nodeId;
+      let clockseq = options.clockseq !== undefined
+        ? options.clockseq
+        : _clockseq;
+      if (node == null || clockseq == null) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const seedBytes = options.random ||
+          options.rng ||
+          crypto.getRandomValues(new Uint8Array(16));
+        if (node == null) {
+          node = _nodeId = [
+            seedBytes[0] | 0x01,
+            seedBytes[1],
+            seedBytes[2],
+            seedBytes[3],
+            seedBytes[4],
+            seedBytes[5],
+          ];
+        }
+        if (clockseq == null) {
+          clockseq = _clockseq = ((seedBytes[6] << 8) | seedBytes[7]) & 0x3fff;
+        }
+      }
+      let msecs = options.msecs !== undefined
+        ? options.msecs
+        : new Date().getTime();
+      let nsecs = options.nsecs !== undefined ? options.nsecs : _lastNSecs + 1;
+      const dt = msecs - _lastMSecs + (nsecs - _lastNSecs) / 10000;
+      if (dt < 0 && options.clockseq === undefined) {
+        clockseq = (clockseq + 1) & 0x3fff;
+      }
+      if ((dt < 0 || msecs > _lastMSecs) && options.nsecs === undefined) {
+        nsecs = 0;
+      }
+      if (nsecs >= 10000) {
+        throw new Error("Can't create more than 10M uuids/sec");
+      }
+      _lastMSecs = msecs;
+      _lastNSecs = nsecs;
+      _clockseq = clockseq;
+      msecs += 12219292800000;
+      const tl = ((msecs & 0xfffffff) * 10000 + nsecs) % 0x100000000;
+      b[i++] = (tl >>> 24) & 0xff;
+      b[i++] = (tl >>> 16) & 0xff;
+      b[i++] = (tl >>> 8) & 0xff;
+      b[i++] = tl & 0xff;
+      const tmh = ((msecs / 0x100000000) * 10000) & 0xfffffff;
+      b[i++] = (tmh >>> 8) & 0xff;
+      b[i++] = tmh & 0xff;
+      b[i++] = ((tmh >>> 24) & 0xf) | 0x10;
+      b[i++] = (tmh >>> 16) & 0xff;
+      b[i++] = (clockseq >>> 8) | 0x80;
+      b[i++] = clockseq & 0xff;
+      for (let n = 0; n < 6; ++n) {
+        b[i + n] = node[n];
+      }
+      return buf ? buf : _common_ts_1.bytesToUuid(b);
+    }
+    exports_2("generate", generate);
+    return {
+      setters: [
+        function (_common_ts_1_1) {
+          _common_ts_1 = _common_ts_1_1;
+        },
+      ],
+      execute: function () {
+        UUID_RE = new RegExp(
+          "^[0-9a-f]{8}-[0-9a-f]{4}-1[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
+          "i",
+        );
+        _lastMSecs = 0;
+        _lastNSecs = 0;
+      },
+    };
+  },
+);
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+System.register(
+  "https://deno.land/std@0.41.0/uuid/v4",
+  ["https://deno.land/std@0.41.0/uuid/_common"],
+  function (exports_3, context_3) {
+    "use strict";
+    var _common_ts_2, UUID_RE;
+    var __moduleName = context_3 && context_3.id;
+    function validate(id) {
+      return UUID_RE.test(id);
+    }
+    exports_3("validate", validate);
+    function generate() {
+      const rnds = crypto.getRandomValues(new Uint8Array(16));
+      rnds[6] = (rnds[6] & 0x0f) | 0x40; // Version 4
+      rnds[8] = (rnds[8] & 0x3f) | 0x80; // Variant 10
+      return _common_ts_2.bytesToUuid(rnds);
+    }
+    exports_3("generate", generate);
+    return {
+      setters: [
+        function (_common_ts_2_1) {
+          _common_ts_2 = _common_ts_2_1;
+        },
+      ],
+      execute: function () {
+        UUID_RE = new RegExp(
+          "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
+          "i",
+        );
+      },
+    };
+  },
+);
+System.register(
+  "https://deno.land/std@0.41.0/uuid/mod",
+  [
+    "https://deno.land/std@0.41.0/uuid/v1",
+    "https://deno.land/std@0.41.0/uuid/v4",
+  ],
+  function (exports_4, context_4) {
+    "use strict";
+    var v1, v4, NIL_UUID, NOT_IMPLEMENTED, v3, v5;
+    var __moduleName = context_4 && context_4.id;
+    function isNil(val) {
+      return val === NIL_UUID;
+    }
+    exports_4("isNil", isNil);
+    return {
+      setters: [
+        function (v1_1) {
+          v1 = v1_1;
+        },
+        function (v4_1) {
+          v4 = v4_1;
+        },
+      ],
+      execute: function () {
+        exports_4("v1", v1);
+        exports_4("v4", v4);
+        exports_4(
+          "NIL_UUID",
+          NIL_UUID = "00000000-0000-0000-0000-000000000000",
+        );
+        NOT_IMPLEMENTED = {
+          generate() {
+            throw new Error("Not implemented");
+          },
+          validate() {
+            throw new Error("Not implemented");
+          },
+        };
+        // TODO Implement
+        exports_4("v3", v3 = NOT_IMPLEMENTED);
+        // TODO Implement
+        exports_4("v5", v5 = NOT_IMPLEMENTED);
+      },
+    };
+  },
+);
+System.register(
+  "file:///Users/nathan/Projects/Deno/uuid-bug/index",
+  ["https://deno.land/std@0.41.0/uuid/mod"],
+  function (exports_5, context_5) {
+    "use strict";
+    var mod_ts_1, uuid;
+    var __moduleName = context_5 && context_5.id;
+    return {
+      setters: [
+        function (mod_ts_1_1) {
+          mod_ts_1 = mod_ts_1_1;
+        },
+      ],
+      execute: function () {
+        uuid = mod_ts_1.v4.generate();
+        console.log("uuid", uuid);
+      },
+    };
+  },
+);
+
+__instantiate("file:///Users/nathan/Projects/Deno/uuid-bug/index");
---
+++ index.ts
@@ -0,0 +1,5 @@
+import { v4 } from "https://deno.land/std@0.41.0/uuid/mod.ts";
+
+const uuid = v4.generate();
+
+console.log("uuid", uuid);
